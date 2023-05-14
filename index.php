f57688342010b7ce56bae9a2dcffeb4a9f0e4ea1
ThomCo25 <91389609+ThomCo25@users.noreply.github.com>
2023-05-14 22:59:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Diapasonia</title>
+	<link rel="stylesheet" href="style.scss">
+	<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<h1>Diapasonia</h1>
+		<nav>
+			<div class="li_nav">
+			<ul>
+				<li class="li_nav"><a href="#">Accueil</a></li>
+				<li class="li_nav"><a href="#">Quiz</a></li>
+				<li class="li_nav"><a href="#">Jeux</a></li>
+				<li class="li_nav"><a href="#">Activités</a></li>
+			</ul>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<section>
+			<h2>Section 1</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus diam vitae libero ullamcorper tristique. Nunc dapibus eu magna a vehicula. Praesent vel leo et enim sodales iaculis. Fusce sed felis eros. </p>
+		</section>
+		<section>
+			<h2>Section 2</h2>
+			<p>Aliquam eget purus ut est laoreet feugiat sed sit amet nisl. Integer semper a nisi nec fringilla. Nulla facilisi. Mauris eget felis ultricies, aliquam quam sit amet, sodales tellus. Vivamus nec ipsum eros.</p>
+		</section>
+	</main>
+	<footer>
+		<p>Copyright © 
+			<!--<script> document.write(new Date().getFullYear()); </script>-->
+			Diapasonia, Tous droits réservés.</p>
+	</footer>
+	<!--<script src="script.js"></script>-->
+</body>
+</html>
